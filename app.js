a14567411b98fb5692cbcc21b841839acc7a7e5e
Gerald Ezenagu <egerald344@gmail.com>
2022-10-04 22:12:07 +0100

    typescript

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('Code goes here');
+function addi(n1, n2) {
+    return n1 + n2;
+}
+var nummber3 = 5;
+var nummber4 = 2.9;
+var results = addi(nummber3, nummber4);
+console.log(results);
--- app.ts
+++ app.ts
@@ -1,1 +1,10 @@
-console.log('Code goes here');
+function addi(n1: number, n2: number) {
+    return n1 + n2
+}
+
+const nummber3= '5';
+const nummber4 = 2.9
+
+const results = addi(nummber3, nummber4)
+
+console.log(results) 
